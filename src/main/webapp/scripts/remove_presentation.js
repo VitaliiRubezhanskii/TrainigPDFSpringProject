de9393b901fa3ee804d2650ed0f5de78e32a277b
shaulid <shauli.daon@gmail.com>
2015-09-06 13:07:28 +0300

    slidepipertest automatic commit

--- src/main/webapp/scripts/remove_presentation.js
+++ src/main/webapp/scripts/remove_presentation.js
@@ -28,39 +28,51 @@ $("#removePresentationButton")
 						
 						if (pres1.length != 1) {
 							swal("Can't do it", "The system currently supports removing one presentation at a time.", "error");
-						} else {
-							// get email
-							var salesmanEmail = getCookie("SalesmanEmail").toLowerCase();
-							datajson = '{"action":"deletePresentation", "salesman_email":"'
-									+ salesmanEmail.toLowerCase()
-									+ '", "presentation":"'
-									+ pres1 + '"}';
-							console.log("removing cust datajson=" + datajson);
-							showWaitMsg();
-							$
-									.ajax({
-										type : "POST",
-										url : "ManagementServlet",
-										data : datajson,
-										contentType : "application/json; charset=utf-8",
-										processData : false,
-										error : function(XmlHttpRequest,
-												status, error) {
-											swal('error from returned json remove cust'
-													+ error);
-										},
-										success : function(msg) {
-											swal("It's been done.", "Presentation deleted!", "success" );
-											console
-													.log("remove pres ajax returned successfully");
-											fillCustomersAndPresentations();
-											// fill cust and pres will set it to true and remove waiting widget.
-											presentationsloaded=false;
-										}
-									});
-						}
+						} else 
+						{
+							var confirm = prompt("To confirm deleting the presentation, type CONFIRM in the textbox. This will break all the links you've sent for this presentation.", "type here confirm");
+							if (confirm != null) 							
+							{							    
+										if (confirm.toLowerCase() == "confirm")
+											{
+														// get email
+														var salesmanEmail = getCookie("SalesmanEmail").toLowerCase();
+														datajson = '{"action":"deletePresentation", "salesman_email":"'
+																+ salesmanEmail.toLowerCase()
+																+ '", "presentation":"'
+																+ pres1 + '"}';
+														console.log("removing cust datajson=" + datajson);
+														showWaitMsg();
+														$
+																.ajax({
+																	type : "POST",
+																	url : "ManagementServlet",
+																	data : datajson,
+																	contentType : "application/json; charset=utf-8",
+																	processData : false,
+																	error : function(XmlHttpRequest,
+																			status, error) {
+																		swal('error from returned json remove cust'
+																				+ error);
+																	},
+																	success : function(msg) {
+																		swal("It's been done.", "Presentation deleted!", "success" );
+																		console
+																				.log("remove pres ajax returned successfully");
+																		fillCustomersAndPresentations();
+																		// fill cust and pres will set it to true and remove waiting widget.
+																		presentationsloaded=false;
+																	}
+																});
+											}  // if confirmbox has confirm in it
+											else
+											{
+													swal("Presentation delete has been cancelled.");
+											}
+													
+							} // if confirm is not null
+							
+					  } // if we have one presentation selected
+			  }// if confirmed deletion - YES.
+}); // of function.
 
-					} else {
-						x = "You pressed Cancel!";
-					}
-				});
