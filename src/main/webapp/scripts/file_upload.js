012aa93a50e7e01a992410032728c9ef1ae47c65
shaulid <shauli.daon@gmail.com>
2015-09-30 15:36:41 +0300

    slidepipertest automatic commit.

--- src/main/webapp/scripts/file_upload.js
+++ src/main/webapp/scripts/file_upload.js
@@ -1,7 +1,11 @@
 
 
 $('#uploadform').submit(function() {
-	send_salesman_event("UPLOADING_FILE", '0', '0', $("#newpresname").val());
+	
+	//make sure text field is filled with salesman email (not visible)
+	  $("#salesman_email").val(getCookie("SalesmanEmail").toLowerCase());
+	  
+	send_salesman_event("UPLOADING_FILE", '0', '0', $("#newpresname").val() + " " + getCookie("SalesmanEmail").toLowerCase());
 	if ($("#newpresname").val() == "") 
 	{
 		swal("Error",'Empty filename field.',"error");
@@ -11,7 +15,7 @@ $('#uploadform').submit(function() {
 	
 	if ($("#file").val() == "") 
 	{
-		swal("Error",'No file selected.',"error");
+		swal("Error",'No PDF file selected.',"error");
 		return false; // don't submit.
 	}
 	
@@ -20,8 +24,27 @@ $('#uploadform').submit(function() {
 	  showWaitMsg(); //show it for a few seconds. I don't hide it, 
 	  // it will disappear after X sec, or when submission leads to a new page.
 	  
+	  
+	  return true; //true means should do the submit action.
+	});
+
+$('#uploadcustomersform').submit(function() {
 	  //make sure text field is filled with salesman email (not visible)
 	  $("#salesman_email").val(getCookie("SalesmanEmail").toLowerCase());
+	send_salesman_event("UPLOADING_CSV_FILE", '0', '0', "new CSV" + " for email " + getCookie("SalesmanEmail").toLowerCase());
+	
+	if ($("#filecsv").val() == "") 
+	{
+		swal("Error",'No CSV file selected.',"error");
+		return false; // don't submit.
+	}
+	
+	// otherwise: wait msg and submit:
+	
+	  showWaitMsg(); //show it for a few seconds. I don't hide it, 
+	  // it will disappear after X sec, or when submission leads to a new page.
+	  
+
 	  return true; //true means should do the submit action.
 	});
 
