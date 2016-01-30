3f92705fdf2477ac8011a2bf76f149dc5804f50b
Yaniv Friedensohn <yanivf@slidepiper.com>
2016-01-30 07:19:50 +0200

    Initial commit 3 - fix bounce rate query

--- src/main/java/slidepiper/db/Analytics.java
+++ src/main/java/slidepiper/db/Analytics.java
@@ -3,30 +3,16 @@ package slidepiper.db;
 public class Analytics {
   
   public static final String sqlFileData =
-        "SELECT\n"
+      "SELECT\n"
       + "  slides.name AS file_name,\n"
-      + "  msg_id AS file_link,\n"
+      + "  customer_events.msg_id AS file_link,\n"
       + "  SUM(IF(event_name = 'OPEN_SLIDES', 1, 0)) AS sum_open_file,\n"
-      + "\n"
-      + "  /* Calculate the file bounce rate:\n"
-      + "  1. A file has one or more sessions.\n"
-      + "  2. Each session can be bounced if there isn't any VIEW_SLIDE event, or unbounced otherwise.\n"
-      + "  3. A file's bounce rate is calculated by dividing the number of bounced sessions by the\n"
-      + "     total number of sessinos.\n"
-      + "  */\n"
-      + "  (SELECT\n"
-      + "    ((SUM(IF(event_name = 'OPEN_SLIDES', 1, 0)) - SUM(IF(event_name = 'VIEW_SLIDE', 1, 0)))\n"
-      + "        / SUM(IF(event_name = 'OPEN_SLIDES', 1, 0)))\n"
-      + "    FROM (SELECT event_name FROM customer_events GROUP BY msg_id, session_id, event_name)\n"
-      + "        AS customer_events)\n"
-      + "    AS file_bounce_rate,\n"
-      + "\n"
-      + "# Calculate the file average slide view duration.\n"
-      + "SUM(param2float) / SUM(IF(event_name = 'VIEW_SLIDE', 1, 0)) AS file_avg_slide_view_duration\n"
-      + "\n"
+      + "  file_link_bounce_rate_view.file_link_bounce_rate AS file_link_bounce_rate,\n"
+      + "  SUM(param2float) / SUM(IF(event_name = 'VIEW_SLIDE', 1, 0)) AS file_avg_slide_view_duration\n"
       + "FROM customer_events\n"
       + "INNER JOIN msg_info ON customer_events.msg_id = msg_info.id\n"
       + "INNER JOIN slides ON msg_info.slides_id = slides.id\n"
+      + "INNER JOIN file_link_bounce_rate_view ON customer_events.msg_id = file_link_bounce_rate_view.msg_id\n"
       + "WHERE slides.sales_man_email=?\n"
-      + "GROUP BY msg_id";
+      + "GROUP BY customer_events.msg_id";
 }
