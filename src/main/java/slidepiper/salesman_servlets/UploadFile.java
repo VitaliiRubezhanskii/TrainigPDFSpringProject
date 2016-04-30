1b5bdfc5e5bc3fc0216435fa321dd17829ac3270
Yaniv Friedensohn <yanivf@slidepiper.com>
2016-04-30 20:35:33 +0300

    Increase file upload max file size to 100MB
    Also changed slides.file data type from MEDIUMBLOB to LONGBLOB.

--- src/main/java/slidepiper/salesman_servlets/UploadFile.java
+++ src/main/java/slidepiper/salesman_servlets/UploadFile.java
@@ -26,7 +26,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 @SuppressWarnings("serial")
 @WebServlet("/upload-file")
-@MultipartConfig(maxFileSize = 16177215)  // upload file's size up to 16MB
+@MultipartConfig(maxFileSize = 104857600)
 public class UploadFile extends HttpServlet {
     
   protected void doPost(HttpServletRequest request,
