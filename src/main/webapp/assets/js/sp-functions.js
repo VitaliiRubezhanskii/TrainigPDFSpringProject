0655b7cb2f5f7c9108777ce2155893376d0990d0
Yaniv Friedensohn <yanivf@slidepiper.com>
2016-05-03 02:09:27 +0300

    Restore visitors map size and fix intercom installation
    Restore visitors map size setting to its original setting.
    
    Fix Intercom installation by initializing it after sp.config is loaded.

--- src/main/webapp/assets/js/sp-functions.js
+++ src/main/webapp/assets/js/sp-functions.js
@@ -310,6 +310,17 @@ sp = {
           
         });
       }
+      
+    /* Intercom Start */
+    window.intercomSettings = {
+      app_id: "j4o2t486",
+      name: sp.config.salesman.email, // Full name
+      email: sp.config.salesman.email // Email address
+    };
+    
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/j4o2t486';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+    /* Intercom End */
+    
     });
   })(),
   
@@ -1009,8 +1020,6 @@ chart: {
             colors: ['#dcdcdc', '#1ab394'],
             minValue: 0
           },
-          height: 354,
-          width: 450,
           datalessRegionColor: '#dcdcdc',
           displayMode: 'markers',
           legend: 'none',
