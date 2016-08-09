face4600f4d66527e5a16e3c1c3a81d423f56925
Max <maxprais@slidepiper.com>
2016-08-09 15:54:12 +0300

    Create like button widget
    Number of likes in Viewer is per document.
    
    Display number of likes per document and per customer in Marketing and
    Sales Analytics.
    
    New general use DbLayer functions:
    - getSalesmanEmailFromFileHash()
    - getFileHashFromFileLinkHash()

--- src/main/java/slidepiper/db/Analytics.java
+++ src/main/java/slidepiper/db/Analytics.java
@@ -275,4 +275,34 @@ public class Analytics {
     + "AND msg_info.sales_man_email = ?\n"
     + "AND msg_info.slides_id = ?\n"
     + "AND msg_info.customer_email = ?";
+  
+  
+  /**
+   * Get likes count per file.
+   */
+  public static final String sqlFileCountLikes = 
+  		"SELECT\n"
+  	+ "  count(event_name)\n"
+  	+ "FROM customer_events\n"
+  	+ "INNER JOIN msg_info ON msg_info.id = customer_events.msg_id\n"
+  	+ "WHERE event_name = 'VIEWER_WIDGET_LIKE_CLICKED'\n"
+  	+ "AND msg_info.sales_man_email = ?\n"
+  	+ "AND msg_info.slides_id = ?\n"
+  	+ "AND msg_info.customer_email <> '" + ConfigProperties.getProperty("test_customer_email") + "'";
+  
+  
+  /**
+   * Get likes count per customer.
+   */
+  public static final String sqlCustomerCountLikes = 
+  		"SELECT\n"
+  	+ "  count(event_name)\n"
+  	+ "FROM customer_events\n"
+  	+ "INNER JOIN msg_info ON msg_info.id = customer_events.msg_id\n"
+  	+ "WHERE event_name = 'VIEWER_WIDGET_LIKE_CLICKED'\n"
+  	+ "AND msg_info.sales_man_email = ?\n"
+  	+ "AND msg_info.slides_id = ?\n"
+  	+ "AND msg_info.customer_email = ?";
+  
+  
 }
