16af516ea4e88a1708e934782608b98d02f0915c
Yaniv Friedensohn <yanivf@slidepiper.com>
2017-01-16 01:08:00 +0200

    Remove unused files

--- src/main/webapp/js/send_email_address.js
+++
@@ -1,9 +0,0 @@
-
-//send address
-function send_email_address()
-{
-			emailaddr =  $("#emailaddr").val();
-			console.log("Email address is: " + emailaddr);
-			send_text_event("WEB", "SUBSCRIBE", emailaddr);			
-			alert("Thanks for subscribing!");
-}
