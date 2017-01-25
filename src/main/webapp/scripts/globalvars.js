db0f8d9a30d5705f1aa756d5f3e4cfd71f2992d5
Yaniv Friedensohn <yanivf@slidepiper.com>
2017-01-25 20:45:20 +0200

    Remove unused files

--- src/main/webapp/scripts/globalvars.js
+++
@@ -1,9 +0,0 @@
-// everything is defined as not loaded yet.
-customersloaded = false;
-presentationsloaded = false;
-
-// if the timeout for verification is active.
-verifyingactivated=false;
-
-
-loggedin=false;
