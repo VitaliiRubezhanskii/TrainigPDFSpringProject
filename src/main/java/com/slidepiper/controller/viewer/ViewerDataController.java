ff73c2ba02d0063ad4a5649c1a1ac66c07926412
Yaniv Friedensohn <yanivf@slidepiper.com>
2017-07-26 11:39:28 +0300

    fix(viewer): Handle missing sp.viewer cookie

--- src/main/java/com/slidepiper/controller/viewer/ViewerDataController.java
+++ src/main/java/com/slidepiper/controller/viewer/ViewerDataController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
 @RestController
 public class ViewerDataController {
     private ObjectMapper objectMapper;
@@ -20,9 +23,18 @@ public class ViewerDataController {
 
     @CrossOrigin(origins = "*")
     @GetMapping(value = "/viewer/viewer-data")
-    public ObjectNode viewerId(@CookieValue("sp.viewer") String viewer) {
+    public ObjectNode viewerId(@CookieValue(value = "sp.viewer", required = false) String viewerCookie,
+                               HttpServletRequest request) {
         ObjectNode objectNode = objectMapper.createObjectNode();
-        String viewerId = JwtUtils.verify(viewer).getClaim("viewerId").asString();
+
+        String viewerId = "";
+        if (Objects.nonNull(viewerCookie)) {
+            viewerId = JwtUtils.verify(viewerCookie).getClaim("viewerId").asString();
+        } else {
+            // TODO: Add Sentry error method.
+            System.err.println("viewerId is null for user agent: " + request.getHeader("User-Agent"));
+        }
+
         objectNode.put("viewerId", viewerId);
 
         return objectNode;
