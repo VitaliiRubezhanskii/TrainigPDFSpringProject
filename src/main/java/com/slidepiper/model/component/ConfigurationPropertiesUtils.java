766996d594299b723e9ab68e839ac392b5b66dc4
Yaniv Friedensohn <yanivf@slidepiper.com>
2017-08-27 18:37:15 +0300

    perf(infrastructure): Consolidate and remove unused API keys

--- src/main/java/com/slidepiper/model/component/ConfigurationPropertiesUtils.java
+++ src/main/java/com/slidepiper/model/component/ConfigurationPropertiesUtils.java
@@ -1,6 +1,5 @@
 package com.slidepiper.model.component;
 
-import com.slidepiper.model.component.configurationproperties.AmazonSESCustomerMessage;
 import com.slidepiper.model.component.configurationproperties.AmazonSNSCustomerMessage;
 import com.slidepiper.model.component.configurationproperties.SpringProfiles;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ConfigurationPropertiesUtils {
+    public static AmazonSNSCustomerMessage amazonSNSCustomerMessage;
+    public static SpringProfiles springProfiles;
 
-  public static AmazonSESCustomerMessage amazonSESCustomerMessage;
-  public static AmazonSNSCustomerMessage amazonSNSCustomerMessage;
-  public static SpringProfiles springProfiles;
-  
-  @Autowired
-  public ConfigurationPropertiesUtils(AmazonSESCustomerMessage amazonSESCustomerMessage,
-      AmazonSNSCustomerMessage amazonSNSCustomerMessage,
-      SpringProfiles springProfiles) {
-    ConfigurationPropertiesUtils.amazonSESCustomerMessage = amazonSESCustomerMessage;
-    ConfigurationPropertiesUtils.amazonSNSCustomerMessage = amazonSNSCustomerMessage;
-    ConfigurationPropertiesUtils.springProfiles = springProfiles;
-  }
+    @Autowired
+    public ConfigurationPropertiesUtils(AmazonSNSCustomerMessage amazonSNSCustomerMessage,
+                                        SpringProfiles springProfiles) {
+        ConfigurationPropertiesUtils.amazonSNSCustomerMessage = amazonSNSCustomerMessage;
+        ConfigurationPropertiesUtils.springProfiles = springProfiles;
+    }
 }
